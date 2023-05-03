b0a87741363f09b2e70aeeaecffa7da4269efc93
kainatbutt121 <kainatbutt121kntkaimabutt@gmail.com>
2023-05-03 21:54:06 +0500

    notification update

--- app/src/main/java/com/abidingtech/pick_by_click/NotificationFragment.java
+++ app/src/main/java/com/abidingtech/pick_by_click/NotificationFragment.java
@@ -14,55 +14,23 @@ import android.view.ViewGroup;
 
 import java.util.ArrayList;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link NotificationFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class NotificationFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-    private ArrayList<News> newsArrayList;
-    private String[] newsHeading;
-    private int[] imageResourceID;
-    private RecyclerView recyclerview;
+
+
 
     public NotificationFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment BlankFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static NotificationFragment newInstance(String param1, String param2) {
-        NotificationFragment fragment = new NotificationFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
@@ -72,47 +40,6 @@ public class NotificationFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_notification, container, false);
     }
 
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        dataInitialize();
-        recyclerview=view.findViewById(R.id.recyclerview);
-        recyclerview.setLayoutManager(new LinearLayoutManager(getContext()));
-        recyclerview.setHasFixedSize(true);
-        MyAdapter myAdapter=new MyAdapter(getContext(),newsArrayList);
-        recyclerview.setAdapter(myAdapter);
-        myAdapter.notifyDataSetChanged();
-    }
-
-    private void dataInitialize() {
-        newsArrayList = new ArrayList<>();
-        newsHeading = new String[]{
-                getString(R.string.head_1),
-                getString(R.string.head_2),
-                getString(R.string.head_3),
-                getString(R.string.head_4),
-                getString(R.string.head_5),
 
 
-
-        };
-        imageResourceID = new int[]
-                {
-                        R.drawable.a,
-        R.drawable.b,
-        R.drawable.c,
-        R.drawable.d,
-        R.drawable.f,
-
-
-
-                };
-
-        for (int i = 0; i < newsHeading.length; i++) {
-
-            News news = new News(newsHeading[i],imageResourceID[i]);
-            newsArrayList.add(news);
-        }
-
-    }
 }
