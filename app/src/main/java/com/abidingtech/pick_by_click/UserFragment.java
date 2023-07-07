18fab0d79676088e3f68423e7ce1523b8ec8da98
kainatbutt121 <kainatbutt121kntkaimabutt@gmail.com>
2023-07-07 13:30:34 +0500

    gallery

--- app/src/main/java/com/abidingtech/pick_by_click/UserFragment.java
+++ app/src/main/java/com/abidingtech/pick_by_click/UserFragment.java
@@ -51,7 +51,9 @@ public class UserFragment extends Fragment {
     Button btnSigout;
 
     Button btnSignout;
+
 //    SigninActivity signinActivity;
+
     FirebaseAuth auth;
     FirebaseUser user;
     public UserFragment() {
@@ -76,11 +78,10 @@ public class UserFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 ImagePicker.with(UserFragment.this)
-                        .galleryOnly()
                         .cropSquare()
                         .crop()	    			//Crop image(Optional), Check Customization for more option
-                        .compress(1024)//Final image size will be less than 1 MB(Optional)
-                        .maxResultSize(1080, 1080)	//Final image resolution will be less than 1080 x 1080(Optional)
+                        .compress(512)//Final image size will be less than 1 MB(Optional)
+                        .maxResultSize(512, 512)	//Final image resolution will be less than 1080 x 1080(Optional)
                         .start();
             }
         });
