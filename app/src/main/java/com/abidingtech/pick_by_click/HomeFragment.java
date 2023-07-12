cc939d05423bded288572331586c595cc5d6c011
xahrakhaliqferoz <xahrakhaliqferoz3122@gmail.com>
2023-07-12 12:46:58 +0500

    UserManualAdded

--- app/src/main/java/com/abidingtech/pick_by_click/HomeFragment.java
+++ app/src/main/java/com/abidingtech/pick_by_click/HomeFragment.java
@@ -1,18 +1,19 @@
 package com.abidingtech.pick_by_click;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.Button;
 
 
 public class HomeFragment extends Fragment {
-
-
+    CardView cardSettings;
 
 
 
@@ -34,6 +35,18 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        //return inflater.inflate(R.layout.fragment_home, container, false);
+        View view=inflater.inflate(R.layout.fragment_home,container,false);
+        cardSettings= cardSettings.findViewById(R.id.settingCard);
+        cardSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), UserManualActivity.class);
+                startActivity(intent);
+                getActivity().finish();
+            }
+             }
+        );
+return view;
+    };
     }
-}
---
+++ app/src/main/java/com/abidingtech/pick_by_click/UserManualActivity.java
@@ -0,0 +1,13 @@
+package com.abidingtech.pick_by_click;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UserManualActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_manual);
+    }
+}
