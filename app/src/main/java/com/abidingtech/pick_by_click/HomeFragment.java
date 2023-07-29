b4d538908db64d39954b91c80f08315715c310f4
kainatbutt121 <kainatbutt121kntkaimabutt@gmail.com>
2023-07-29 12:16:13 +0500

    All Done

--- app/src/main/java/com/abidingtech/pick_by_click/HomeFragment.java
+++ app/src/main/java/com/abidingtech/pick_by_click/HomeFragment.java
@@ -21,34 +21,29 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+
 public class HomeFragment extends Fragment {
     CardView cardSettings;
     TextView tvUserName;
+    CardView registerationCard;
 
-<<<<<<< HEAD
+    private FirebaseUser User;
+    private DatabaseReference reference;
+
+    private String userID;
+
     public HomeFragment() {
         // Required empty public constructor
     }
-=======
-    public class HomeFragment extends Fragment {
-        CardView cardSettings;
-        TextView tvUserName;
-        CardView registerationCard;
-
-        private FirebaseUser User;
-        private DatabaseReference reference;
-
-        private String userID;
->>>>>>> bad3df55350d58ae7f8cb9b008bad4c1f24e9af7
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
         cardSettings = view.findViewById(R.id.settingCard);
+        registerationCard=view.findViewById(R.id.RegisterationCard);
         tvUserName = view.findViewById(R.id.tvUserName);
 
-<<<<<<< HEAD
         cardSettings.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -56,15 +51,13 @@ public class HomeFragment extends Fragment {
                 startActivity(intent);
             }
         });
-=======
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View view = inflater.inflate(R.layout.fragment_home, container, false);
-            cardSettings = view.findViewById(R.id.settingCard);
-            registerationCard=view.findViewById(R.id.RegisterationCard);
-            tvUserName = view.findViewById(R.id.tvUserName);
->>>>>>> bad3df55350d58ae7f8cb9b008bad4c1f24e9af7
+        registerationCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), RegisterDevice.class);
+                startActivity(intent);
+            }
+        });
 
         FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
         if (currentUser != null) {
@@ -72,79 +65,29 @@ public class HomeFragment extends Fragment {
             String userId = currentUser.getUid();
 
             // Get a reference to the Firebase Realtime Database
-            DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("users").child(userId);
+            DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("Users").child(userId);
 
             // Listen for changes in the user's data
             databaseReference.addValueEventListener(new ValueEventListener() {
+                @SuppressLint("SetTextI18n")
                 @Override
-<<<<<<< HEAD
                 public void onDataChange(DataSnapshot dataSnapshot) {
-                    // Check if the 'name' field exists in the database for the user
-                    if (dataSnapshot.hasChild("Users")) {
-                        // Retrieve the user's name from the database
-                        String userName = dataSnapshot.child("Users").getValue(String.class);
+                    // Retrieve the user's name from the database
+                    String name = dataSnapshot.child("name").getValue(String.class);
 
-                        // Update the TextView with the user's name
-                        tvUserName.setText( userName + "!");
-                    } else {
-                        // If the 'name' field does not exist in the database for the user,
-                        // you can set a default greeting or handle the case as appropriate.
-                        tvUserName.setText("Hello, User!");
-=======
-                public void onClick(View v) {
-                    Intent intent = new Intent(getActivity(), UserManualActivity.class);
-                    startActivity(intent);
-                }
-            });
-            registerationCard.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent intent = new Intent(getActivity(), RegisterDevice.class);
-                    startActivity(intent);
-                }
-            });
-
-            FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
-            if (currentUser != null) {
-                // Get the currently logged-in user's UID
-                String userId = currentUser.getUid();
-
-                // Get a reference to the Firebase Realtime Database
-                DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("Users").child(userId);
-
-                // Listen for changes in the user's data
-                databaseReference.addValueEventListener(new ValueEventListener() {
-                    @SuppressLint("SetTextI18n")
-                    @Override
-                    public void onDataChange(DataSnapshot dataSnapshot) {
-                        // Retrieve the user's name from the database
-                        String name = dataSnapshot.child("name").getValue(String.class);
-
-                        // Update the TextView with the user's name
-                        tvUserName.setText("Hello, " + name + "!");
->>>>>>> bad3df55350d58ae7f8cb9b008bad4c1f24e9af7
-                    }
+                    // Update the TextView with the user's name
+                    tvUserName.setText("Hello, " + name + "!");
                 }
 
-<<<<<<< HEAD
                 @Override
                 public void onCancelled(DatabaseError databaseError) {
                     // Handle database read error
                 }
             });
-=======
-                    @Override
-                    public void onCancelled(DatabaseError databaseError) {
-                        // Handle database read error
-                    }
-                });
-            }
-
-            return view;
-
->>>>>>> bad3df55350d58ae7f8cb9b008bad4c1f24e9af7
         }
 
         return view;
+
     }
+
 }
