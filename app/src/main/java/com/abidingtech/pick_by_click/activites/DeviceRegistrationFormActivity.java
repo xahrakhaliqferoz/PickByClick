d90560cd2935b54d82b6613849031884c2796e61
kainatbutt121 <kainatbutt121kntkaimabutt@gmail.com>
2023-08-09 20:25:55 +0500

    DeviceSaveChecks

--- app/src/main/java/com/abidingtech/pick_by_click/activites/DeviceRegistrationFormActivity.java
+++ app/src/main/java/com/abidingtech/pick_by_click/activites/DeviceRegistrationFormActivity.java
@@ -1,4 +1,3 @@
-
 package com.abidingtech.pick_by_click.activites;
 
 import androidx.annotation.NonNull;
@@ -25,37 +24,39 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 public class DeviceRegistrationFormActivity extends AppCompatActivity {
-CardView cardView,imageCard;
-EditText name,id;
-Button savebtn;
-ImageView cardIM;
+    CardView cardView, imageCard;
+    EditText name, id;
+    Button savebtn;
+    ImageView cardIM;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_device_registration_form);
-        cardView=findViewById(R.id.cardView);
-        imageCard=findViewById(R.id.imageCard);
-        name=findViewById(R.id.name);
-        id=findViewById(R.id.id);
-        cardIM=findViewById(R.id.cardIM);
-        savebtn=findViewById(R.id.savebtn);
+        cardView = findViewById(R.id.cardView);
+        imageCard = findViewById(R.id.imageCard);
+        name = findViewById(R.id.name);
+        id = findViewById(R.id.id);
+        cardIM = findViewById(R.id.cardIM);
+        savebtn = findViewById(R.id.savebtn);
+
         savebtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 String deviceName = name.getText().toString().trim();
                 String deviceId = id.getText().toString().trim();
 
-
-                // Check if the fields are not empty
-                if (!deviceName.isEmpty() && !deviceId.isEmpty()) {
-                    // Get a reference to the Firebase Realtime Database and the "Devices" node
+                if (deviceName.isEmpty()) {
+                    name.setError("Device name is required");
+                } else if (deviceId.isEmpty()) {
+                    id.setError("Device ID is required");
+                } else {
+                    // Both fields are filled, proceed with saving
                     DatabaseReference devicesRef = FirebaseDatabase.getInstance()
                             .getReference("Devices")
                             .child(FirebaseAuth.getInstance().getUid())
                             .child(deviceId);
 
-                    // Create a new Device object with the entered data
                     Device device = new Device(deviceName, deviceId);
                     devicesRef.setValue(device).addOnSuccessListener(new OnSuccessListener<Void>() {
                         @Override
@@ -64,16 +65,8 @@ ImageView cardIM;
                             finish();
                         }
                     });
-
-                } else {
-                    Toast.makeText(DeviceRegistrationFormActivity.this, "Please fill all the fields", Toast.LENGTH_SHORT).show();
                 }
-                startActivity(new Intent(DeviceRegistrationFormActivity.this, RegisterDeviceActivity.class));
-                finish();
             }
         });
-
-
-
     }
 }
